5ba14d254d84cbacf576f660220acbbe70e3487a
exbrute <exbrutee@gmail.com>
2025-10-15 16:20:24 +0300

    Create table.php

---
+++ table.php
@@ -0,0 +1,15 @@
+<style>
+
+.tablebtn{
+    
+    background-color:  #e5f2ff;
+    width: 10%;
+    border-radius: 1.5rem;
+    height: 5%;
+}
+
+</style>
+
+<div class="tablebtn">
+    Все игры
+</div>
